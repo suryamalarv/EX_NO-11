8617860f3f5e1e9ee8b7a8bc40fa214f987cba64
suryamalarv <145742486+suryamalarv@users.noreply.github.com>
2025-11-24 13:38:08 +0530

    Add files via upload

---
+++ huffman_coding.py
@@ -0,0 +1,18 @@
+
+
+# Get the input String
+string =
+
+# Create tree nodes
+class NodeTree(object):
+
+# Main function to implement huffman coding
+def huffman_code_tree(node, left=True, binString=''):
+
+# Calculate frequency of occurrence
+freq = {}
+
+# Print the characters and its huffmancode
+huffmanCode = huffman_code_tree(nodes[0][0])
+
+print(' Char | Huffman code ')
